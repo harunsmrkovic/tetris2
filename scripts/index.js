e0bb368a83d3752ae7a33fdffb0b2d05424e8b6e
Harun Smrkovic <harun@personal.com>
2016-05-28 16:32:16 +0200

    separate draw and tetris

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,32 @@
 import game from './canvasInit'
 
+const Drawing = ({ canvas, canvasWidth, canvasHeight }) => {
+
+  const drawOutline = (x, y) => {
+    canvas.strokeRect(x, y, 50, 50)
+  }
+
+  const drawRect = (x, y, color = '200, 0, 0') => {
+    canvas.fillStyle = 'rgb(' + color + ')'
+    canvas.fillRect(x, y, 50, 50)
+  }
+
+  const clearCanvas = () => {
+    canvas.clearRect(0, 0, canvasWidth, canvasHeight)
+  }
+
+  return {
+    drawOutline,
+    drawRect,
+    clearCanvas
+  }
+}
+
 const Tetris = ({ game, canvasWidth, canvasHeight, blockWidth = 25, blockHeight = 25 }) => {
 
   let matrix = [];
+
+  const drawing = Drawing({ canvas: game, canvasWidth, canvasHeight })
 
   const initMatrix = () => {
     matrix = [...Array(canvasWidth / blockWidth)].map(_ => [...Array(canvasHeight / blockHeight)].map(_ => false))
@@ -16,30 +40,17 @@ const Tetris = ({ game, canvasWidth, canvasHeight, blockWidth = 25, blockHeight 
     }
   }
 
-  const drawOutlines = (color = '#dddddd') => {
+  const initOutlines = (color = '#dddddd') => {
     game.strokeStyle = color
     const outlineCoords = _.flatten(matrix.map((ys, x) => ys.map((s, y) => getCoordinates(x, y))))
-    _.each(outlineCoords, (coord) => drawOutline(coord.x, coord.y))
-  }
-
-  const drawOutline = (x, y) => {
-    game.strokeRect(x, y, 50, 50)
-  }
-
-  const drawRect = (x, y, color = '200, 0, 0') => {
-    game.fillStyle = 'rgb(' + color + ')'
-    game.fillRect(x, y, 50, 50)
-  }
-
-  const clearCanvas = () => {
-    game.clearRect(0, 0, canvasWidth, canvasHeight)
+    _.each(outlineCoords, (coord) => drawing.drawOutline(coord.x, coord.y))
   }
 
   // Public
 
   const start = () => {
     initMatrix()
-    drawOutlines()
+    initOutlines()
   }
 
   return {
